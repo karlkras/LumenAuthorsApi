f1f55248733c2248cf914bfc9be6c2d2c73b82df
karlkras <kkrasnowsky@llts.com>
2019-04-21 10:45:12 -0700

    completion of sec2lect9

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Return the full list of authors
+     * @return Illuminate\Http\Response
+     */
+    public function index() {
+
+    }
+
+    /**
+     * Create an author
+     * @param Request $request
+     * @return Illuminate\Http\Response
+     */
+    public function store(Request $request) {
+
+    }
+
+    /**
+     * Provides details of existing author
+     * @param $author
+     * @return Illuminate\Http\Response
+     */
+    public function show($author) {
+
+    }
+
+    /**
+     * Updates an existing author information
+     * @param Request $request
+     * @param $author
+     * @return Illuminate\Http\Response
+     */
+    public function update(Request $request, $author) {
+
+    }
+
+    /**
+     * Deletes an existing author
+     * @param $author
+     * @return Illuminate\Http\Response
+     */
+    public function destroy($author) {
+
+    }
+}
